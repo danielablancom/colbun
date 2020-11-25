52363480d08fd936322d3c4c4b8ca703b4b133e7
Nathiuk <nathaliasaray0220@gmail.com>
2020-11-25 10:50:20 -0500

    agregando import bootstrap

--- src/Components/Temas/Temas.jsx
+++ src/Components/Temas/Temas.jsx
@@ -1,12 +1,75 @@
 import React, { Fragment } from 'react'; 
-import Temas from '../Temas/Temas';
 import Temas from '../Temas/Temas.css';
+import logo from '../Assets/Image/logo.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Temas = () => {
-    return (
-    <Fragment>
-        
-    </Fragment>
-    );
+	return (
+	<Fragment>
+	<div className="logo">	
+		<img src={logo}/>
+	</div>
+<form className="form-inline">
+	<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+
+<div className="select">     
+	<select>
+		<option value=""></option>
+		<option value="temas">Temas</option>
+	</select>
+	<select>
+		<option value=""></option>
+		<option value="tipos entregables">Tipos entregables</option>
+	</select>
+	<select>
+		<option value=""></option>
+		<option value="areas">Áreas</option>
+	</select>
+	<select>
+		<option value=""></option>
+		<option value="tipos de opciones">Tipos de opciones</option>
+	</select>
+	<select>
+		<option value=""></option>
+		<option value="nivel autorizacion">Nivel autorización</option>
+	</select>
+</div>
+
+<table className="table">
+<thead>
+	<tr>
+	<th scope="col">Fecha</th>
+	<th scope="col">Nombre</th>
+	<th scope="col">Tema</th>
+	<th scope="col">Area cliente</th>
+	<th scope="col">Area relacionada</th>
+	<th scope="col">Opciones</th>
+	</tr>
+</thead>
+<tbody>
+	<tr>
+	<th scope="row">1</th>
+	<td>Mark</td>
+	<td>Otto</td>
+	<td>@mdo</td>
+	</tr>
+	<tr>
+	<th scope="row">2</th>
+	<td>Jacob</td>
+	<td>Thornton</td>
+	<td>@fat</td>
+	</tr>
+	<tr>
+	<th scope="row">3</th>
+	<td>Larry</td>
+	<td>the Bird</td>
+	<td>@twitter</td>
+	</tr>
+</tbody>
+</table>
+	</Fragment>
+	);
 }
 export default Temas;
