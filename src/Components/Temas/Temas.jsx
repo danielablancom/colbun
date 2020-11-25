5e9d9d974cfa26606674d2ff246ea6addefe3439
Nathiuk <nathaliasaray0220@gmail.com>
2020-11-25 12:19:33 -0500

    Estilo imagen

--- src/Components/Temas/Temas.jsx
+++ src/Components/Temas/Temas.jsx
@@ -1,18 +1,11 @@
 import React, { Fragment } from 'react'; 
 import Temas from '../Temas/Temas.css';
-import logo from '../Assets/Image/logo.jpg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Temas = () => {
 	return (
 	<Fragment>
-	<div className="logo">	
-		<img src={logo}/>
-	</div>
-<form className="form-inline">
-	<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-	<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
+	
 
 <div className="select">     
 	<select>
